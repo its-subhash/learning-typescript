586ac23d0d40a81fcaf71936cb519f5a1373646c
Darsh Mishra <subhashmishra4322@gmail.com>
2025-05-05 23:53:01 +0530

    05052025 UPDATE learned about classes maybe

--- puretypescript/dist/index.js
+++ puretypescript/dist/index.js
@@ -1,7 +1,6 @@
-console.log("Radhe Radhe...")
-
-let a:number =45;
-
+"use strict";
+console.log("Radhe Radhe...");
+let a = 45;
 // class User {
 //     email:string;
 //     public name:string;
@@ -11,20 +10,14 @@ let a:number =45;
 //         this.name = name;
 //     }
 // }
-
-
 class User {
-    private city: string = " " // you can access it inside class only. everything else which isn't marked as privet is always public.
-    constructor(private email:string, public name:string){
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = " "; // you can access it inside class only. everything else which isn't marked as privet is always public.
         this.email = email;
         this.name = name;
     }
 }
-
-
-
-
-const darsh = new User ("darsh@com","Darsh")
-
-console.log(darsh.email)
-
+const darsh = new User("darsh@com", "Darsh");
+console.log(darsh.email);
