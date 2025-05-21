59bcf16d7cb46c865ead3bc3d2c2e5c3e6f27d5e
Darsh Mishra <subhashmishra4322@gmail.com>
2025-05-21 19:04:03 +0530

    210525 UPDATE

--- refreshing-js-usingj-ts/dist/myobjectprac.js
+++ refreshing-js-usingj-ts/dist/myobjectprac.js
@@ -4,25 +4,68 @@ const btnTail = document.getElementById("btn-tail");
 const btnReset = document.getElementById("btn-reset");
 let playerScore = document.getElementById("player-score");
 let computerScore = document.getElementById("computer-score");
-let flips = document.getElementById("flips");
+let flipElement = document.getElementById("flips");
 let message = document.getElementById("message");
 let scoreBoard = {
-    computer: 0,
-    player: 0,
-    flips: 0,
+    computer: 4,
+    player: 5,
+    flipCount: 10,
 };
-let board;
-const getData = () => {
-    board = localStorage.getItem("coin-flipi");
+const dbName = "coinFlipp";
+const initializeDB = () => {
+    let board = localStorage.getItem(dbName);
     if (board) {
-        scoreBoard = JSON.parse(board);
         console.log("DataBase Detected...");
     }
     else if (!board) {
-        localStorage.setItem("coin-flipi", JSON.stringify(scoreBoard));
-        console.log("DataBase Created...");
+        localStorage.setItem(dbName, JSON.stringify(scoreBoard));
     }
     else {
         console.log("Nothing happend");
     }
 };
+initializeDB();
+// getting data from DB
+const getDB = () => {
+    const stored = localStorage.getItem(dbName);
+    if (stored != null) {
+        try {
+            const { computer, player, flipCount } = JSON.parse(stored);
+            return [computer, player, flipCount];
+        }
+        catch (e) {
+            console.log(`something went wrong ${e}`);
+            return [-1, -1, -1];
+        }
+    }
+    console.log("There is some error...please check");
+    return [-1, -1, -1];
+};
+function showMessage(mess) {
+    if (message != undefined) {
+        try {
+            message.innerText = mess;
+        }
+        catch (e) {
+            console.log(`Error Occured: ${e}`);
+        }
+    }
+}
+function playGame(val) {
+    let options = ["heads", "tails"];
+    const flippedCoin = options[Math.round(Math.random())];
+    if (val === flippedCoin) {
+        showMessage(`Coin Flips to ${flippedCoin}, You Win!`);
+    }
+    else {
+        showMessage(`Coin Flips to ${flippedCoin}, You Loose!`);
+    }
+}
+if (btnHead != null) {
+    btnHead.addEventListener('click', () => {
+        playGame('heads');
+    });
+}
+if (btnTail != null) {
+    btnTail.addEventListener('click', () => playGame('tails'));
+}
--- refreshing-js-usingj-ts/src/myobjectprac.ts
+++ refreshing-js-usingj-ts/src/myobjectprac.ts
@@ -3,7 +3,7 @@ const btnTail = document.getElementById("btn-tail");
 const btnReset = document.getElementById("btn-reset");
 let playerScore = document.getElementById("player-score");
 let computerScore = document.getElementById("computer-score");
-let flips = document.getElementById("flips");
+let flipElement = document.getElementById("flips");
 let message = document.getElementById("message");
 
 //initialization
@@ -11,26 +11,73 @@ let message = document.getElementById("message");
 type board = {
   computer: number;
   player: number;
-  flips: number;
+  flipCount: number;
 };
 
 let scoreBoard: board = {
-  computer: 0,
-  player: 0,
-  flips: 0,
+  computer: 4,
+  player: 5,
+  flipCount: 10,
 };
 
-let board;
+const dbName = "coinFlipp";
 
-const getData = () => {
-  board = localStorage.getItem("coin-flipi");
+const initializeDB = () => {
+  let board = localStorage.getItem(dbName);
   if (board) {
-    scoreBoard = JSON.parse(board);
     console.log("DataBase Detected...");
   } else if (!board) {
-    localStorage.setItem("coin-flipi", JSON.stringify(scoreBoard));
-    console.log("DataBase Created...");
+    localStorage.setItem(dbName, JSON.stringify(scoreBoard));
   } else {
     console.log("Nothing happend");
   }
 };
+
+initializeDB();
+
+// getting data from DB
+const getDB = (): [number, number, number] => {
+  const stored = localStorage.getItem(dbName);
+  if (stored != null) {
+    try {
+      const { computer, player, flipCount }: board = JSON.parse(stored);
+      return [computer, player, flipCount];
+    } catch (e) {
+      console.log(`something went wrong ${e}`);
+      return [-1,-1,-1]
+    }
+  }
+  console.log("There is some error...please check")
+  return [-1,-1,-1]
+};
+
+function showMessage (mess:string):void {
+      if (message != undefined) {
+      try {
+        message.innerText = mess;
+      } catch (e) {
+        console.log(`Error Occured: ${e}`);
+      }
+    }
+}
+
+function playGame(val: "heads" | "tails"){
+  let options: ["heads", "tails"] = ["heads", "tails"];
+  const flippedCoin = options[Math.round(Math.random())];
+  if (val === flippedCoin) {
+    showMessage(`Coin Flips to ${flippedCoin}, You Win!`)
+
+  }else{
+    showMessage(`Coin Flips to ${flippedCoin}, You Loose!`)
+  }
+}
+
+
+if (btnHead != null){
+  btnHead.addEventListener('click', ()=>{
+    playGame('heads')})
+}
+if (btnTail != null){
+  btnTail.addEventListener('click', ()=>playGame('tails'))
+}
+
